95e709bcb8eaa12d78372905383d3f5b036338fe
Shami_Mourya <shamimourya9852@gmail.com>
2023-06-17 22:20:09 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
+import './App.css';
+import HomePage from './homepage/homePage';
+import Search from './Search';
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path='/' element = {<HomePage/>}></Route>
+          <Route path='/search' element= {<Search/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
